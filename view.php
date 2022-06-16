ea0599b1d2806c6681cc064ab5e489a92e2841ad
sworc <lightbox@nifty.com>
2022-06-16 14:15:50 +0900

    Update view.php

--- view.php
+++ view.php
@@ -4,7 +4,7 @@
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
-    <title>簡易掲示板 v11 アップロード</title>
+    <title>掲示板 v11 アップロード</title>
     <link rel="stylesheet" href="client.css?_=<?= time() ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css">
